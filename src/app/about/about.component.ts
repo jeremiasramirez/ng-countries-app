948f5c3679626ac47288ab926ae42e1613688b16
Jeremias ramirez <jereramirez499@gmail.com>
2020-04-04 00:38:38 -0400

    mostrado de todos los paises

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { timer }from 'rxjs'
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class AboutComponent   {
+  public spinner = {
+    off: true
   }
+  constructor() {
+    timer(500).subscribe(timing=>this.spinner.off = false)
+  }
+
+
 
 }
--- src/app/all/all.component.ts
+++ src/app/all/all.component.ts
@@ -5,25 +5,28 @@ import { timer } from 'rxjs'
 @Component({
   selector: 'app-all',
   templateUrl: './all.component.html',
-  styleUrls: ['./all.component.css'],
+  styleUrls: ['./all.component.css','../generalStyle/general.css'],
   providers: [ServiceCountry]
 })
 export class AllComponent  {
    public spinner = {
      off: true
    }
+   public countriesAll : any[] = []
   constructor(public countryService: ServiceCountry) {
+
+    this.chargedCountries();
+
+  }
+
+  chargedCountries(){
 
     this.countryService.getAllCountry().subscribe((resp)=>{
       console.log(resp);
+      this.countriesAll = resp
     },
-    ()=>{
-      return
-    },
-    ()=>{
-      this.spinner.off = false
-    }
-  )
+    ()=>{return},
+    ()=>{this.spinner.off = false})
 
   }
 
