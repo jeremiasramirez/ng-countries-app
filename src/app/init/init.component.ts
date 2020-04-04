6f8bc74c8a7e4d8c38d57868f76250700e2a5e4c
Jeremias ramirez <jereramirez499@gmail.com>
2020-04-04 00:52:05 -0400

    Agregando redireccion a home

--- src/app/init/init.component.ts
+++ src/app/init/init.component.ts
@@ -1,14 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { timer }from 'rxjs'
+import { Router } from "@angular/router"
+
+
 @Component({
   selector: 'app-init',
   templateUrl: './init.component.html',
   styleUrls: ['./init.component.css', '../generalStyle/general.css']
 })
-export class InitComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class InitComponent  {
+  public spinner ={
+    off:true
   }
+  constructor(public router:Router) {
+    timer(1000).subscribe(timing=>this.spinner.off=false)
+  }
+
+  goHome(){
+    timer(400).subscribe(timing=>this.router.navigate(["home"]))
+  }
+
+
 
 }
