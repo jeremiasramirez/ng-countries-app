5ae9e85ef405e568c3ad1ff6354b1f5448160584
Jeremias ramirez <jereramirez499@gmail.com>
2020-04-05 19:20:55 -0400

    Agregando button de explorer en home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,7 +4,7 @@ import { ServiceCountry } from "../services/service.country";
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
+  styleUrls: ['./home.component.css','../generalStyle/general.css'],
   providers: [ServiceCountry]
 })
 export class HomeComponent  {
