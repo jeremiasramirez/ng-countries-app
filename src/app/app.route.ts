bbabe5ac3c6ad44d75ec64717470afbbb3a398fd
Jeremias ramirez <jereramirez499@gmail.com>
2020-04-05 18:59:31 -0400

    agregado componente de explore a rutas + flex end para menu

--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -5,6 +5,7 @@ import { AllComponent } from './all/all.component';
 import { InitComponent } from './init/init.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { ExploreComponent } from './explore/explore.component'
 
 const ROUTES : Routes = [
 
@@ -13,6 +14,7 @@ const ROUTES : Routes = [
   {path: 'search', component: SearchComponent},
   {path: 'search/:name', component: SearchComponent},
   {path: 'all', component: AllComponent},
+  {path: 'explore', component: ExploreComponent},
   {path: 'about', component: AboutComponent},
   {path: '', component: InitComponent},
   {path: '**', component: InitComponent}
