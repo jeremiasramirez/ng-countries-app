4978f1fa3b4270659fb27d1d539cb89baeb8275a
Jeremias ramirez <jereramirez499@gmail.com>
2020-04-19 19:40:59 -0400

    agregando nuevo servicio para explorer component

---
+++ src/app/services/explore.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from "@angular/core"
+import { ajax } from 'rxjs/ajax'
+import { Router } from "@angular/router";
+import { pluck, sampleTime, delay } from 'rxjs/operators'
+import { timer } from 'rxjs'
+@Injectable()
+
+export class ExploreService{
+
+  public urlAllRegion: string = 'assets/api/image-explore.json'
+
+  constructor(public router:Router){
+
+  }
+
+
+  public getAllRegion(){
+
+    return ajax.get(this.urlAllRegion).pipe(
+      delay(1000),
+      pluck('response')
+    )
+
+  }
+
+
+
+
+}
