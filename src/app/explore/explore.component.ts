89367e9daaa8885b4aa1dc53215818e9a26373a3
Jeremias ramirez <jereramirez499@gmail.com>
2020-04-23 20:45:31 -0400

    servicio de languages creado + mostrado de languages

--- src/app/explore/explore.component.ts
+++ src/app/explore/explore.component.ts
@@ -2,47 +2,85 @@ import { Component } from '@angular/core';
 import { ServiceCountry } from "../services/service.country";
 import { ExploreService } from "../services/explore.service";
 import { ContinentService } from "../services/continent.service";
+import { LanguageService } from "../services/language.service";
 import { Theme } from "../services/theme.service";
 
 import {MatSnackBar} from '@angular/material/snack-bar';
 import { timer,concat, Subject ,from} from "rxjs";
 import { delay } from "rxjs/operators";
 import { Router } from "@angular/router";
+
+
 @Component({
   selector: 'app-explore',
   templateUrl: './explore.component.html',
   styleUrls: ['./explore.component.css', '../generalStyle/general.css'],
-  providers: [ServiceCountry, ExploreService, ContinentService, Theme]
+  providers: [
+    ServiceCountry,
+    ExploreService,
+    ContinentService,
+    Theme,
+    LanguageService
+  ]
 })
 export class ExploreComponent {
 
   public allRegion: [];
+
+  public allLanguages : any[] = [];
+
   public allContinent : [];
 
 
   public spinner = {
     off:true
   }
+
   public spinner2 = {
     off:true
   }
+
+  public spinner3 = {
+    off:true
+  }
   constructor(private _snackBar: MatSnackBar,public countryService:ServiceCountry,
-             public exploreService:ExploreService, public continent:ContinentService,
-              public router:Router, public theme:Theme) {
-      this.theme.setColorTheme("theme--orange", "menu--orange")
+              public exploreService:ExploreService, public continent:ContinentService,
+              public router:Router, public theme:Theme, public language:LanguageService) {
+
+      this.theme.setColorTheme("theme--orange", "menu--orange");
+
       timer(800).subscribe(timing=>this.openSnackBar('showing all regions', 'Ok'));
+
       this.setAllContinent();
+
       this.countryService.changeTextNamePage('Explore')
+
       this.setAllRegionToArray();
 
+      this.setAllLanguages()
 
 
 
   }
 
+
+  public setAllLanguages(){
+
+    this.language.getAllForLanguages().subscribe(
+                                    (resp)=>{this.allLanguages = resp;},
+                                    (err)=>{return err},
+                                    ()=>{this.spinner3.off=false})
+
+
+
+  }
+
+
   setAllContinent(){
-    this.continent.getAllContinent().pipe(delay(1000)).subscribe(resp => {this.allContinent = resp;   },
-                                  (err)=>{return err}, ()=>{this.spinner2.off=false})
+    this.continent.getAllContinent().pipe(delay(1000)).subscribe(
+                                  (resp) => {this.allContinent = resp;},
+                                  (err)=>{return err},
+                                  ()=>{this.spinner2.off=false})
   }
   setAllRegionToArray(){
 
--- src/app/services/language.service.ts
+++ src/app/services/language.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from "@angular/core"
-
+import { ajax } from "rxjs/ajax";
+import { delay,pluck } from "rxjs/operators";
 @Injectable()
 
 export class LanguageService{
-
+  public urlForLanguages : string = "assets/api/language.json"
   constructor(){
 
   }
+
+  getAllForLanguages(){
+     return ajax.get(this.urlForLanguages).pipe(delay(300),pluck('response'))
+  }
+
+
 
 }
